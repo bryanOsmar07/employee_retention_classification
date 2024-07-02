c178a24de889c4bf48e19aadcdf1fa4644d0e3a7
bryanOsmar07 <bryanosmar07@gmail.com>
2024-07-01 21:05:35 -0500

    MAC sincro

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import sys
 import json
 from sklearn.impute import KNNImputer
 from src.logger import logging
@@ -26,7 +27,6 @@ class Preprocessor:
     def __init__(self,run_id,data_path,mode):
         self.run_id = run_id
         self.data_path = data_path
-        self.logger = Logger(self.run_id, 'Preprocessor', mode)
 
     def get_data(self):
         """
@@ -34,22 +34,19 @@ class Preprocessor:
         * description: method to read datafile
         * return: A pandas DataFrame
         *
-        * who             when           version  change (include bug# if apply)
-        * ----------      -----------    -------  ------------------------------
-        * bcheekati       05-MAY-2020    1.0      initial creation
         *
         * Parameters
         *   none:
         """
         try:
             # reading the data file
-            self.logger.info('Start of reading dataset...')
+            logging.info('Start of reading dataset...')
             self.data= pd.read_csv(self.data_path+'_validation/InputFile.csv')
-            self.logger.info('End of reading dataset...')
+            logging.info('End of reading dataset...')
             return self.data
         except Exception as e:
-            self.logger.exception('Exception raised while reading dataset: %s'+str(e))
-            raise Exception()
+            logging.exception('Exception raised while reading dataset')
+            raise CustomException(e,sys)
 
     def drop_columns(self,data,columns):
         """
@@ -57,9 +54,6 @@ class Preprocessor:
         * description: method to drop columns
         * return: A pandas DataFrame after removing the specified columns.
         *
-        * who             when           version  change (include bug# if apply)
-        * ----------      -----------    -------  ------------------------------
-        * bcheekati       05-MAY-2020    1.0      initial creation
         *
         * Parameters
         *   data:
@@ -68,13 +62,13 @@ class Preprocessor:
         self.data=data
         self.columns=columns
         try:
-            self.logger.info('Start of Droping Columns...')
+            logging.info('Start of Droping Columns...')
             self.useful_data=self.data.drop(labels=self.columns, axis=1) # drop the labels specified in the columns
-            self.logger.info('End of Droping Columns...')
+            logging.info('End of Droping Columns...')
             return self.useful_data
         except Exception as e:
-            self.logger.exception('Exception raised while Droping Columns:'+str(e))
-            raise Exception()
+            logging.info('Exception raised while Droping Columns')
+            raise CustomException(e,sys)
 
     def is_null_present(self,data):
         """
@@ -91,7 +85,7 @@ class Preprocessor:
         """
         self.null_present = False
         try:
-            self.logger.info('Start of finding missing values...')
+            logging.info('Start of finding missing values...')
             self.null_counts=data.isna().sum() # check for the count of null values per column
             for i in self.null_counts:
                 if i>0:
@@ -102,11 +96,11 @@ class Preprocessor:
                 dataframe_with_null['columns'] = data.columns
                 dataframe_with_null['missing values count'] = np.asarray(data.isna().sum())
                 dataframe_with_null.to_csv(self.data_path+'_validation/'+'null_values.csv') # storing the null column information to file
-            self.logger.info('End of finding missing values...')
+            logging.info('End of finding missing values...')
             return self.null_present
         except Exception as e:
-            self.logger.exception('Exception raised while finding missing values:'+str(e))
-            raise Exception()
+            logging.info('Exception raised while finding missing values')
+            raise CustomException(e,sys)
 
     def impute_missing_values(self, data):
         """
@@ -114,25 +108,22 @@ class Preprocessor:
         * description: method to impute missing values
         * return: none
         *
-        * who             when           version  change (include bug# if apply)
-        * ----------      -----------    -------  ------------------------------
-        * bcheekati       05-MAY-2020    1.0      initial creation
         *
         * Parameters
         *   data:
         """
         self.data= data
         try:
-            self.logger.info('Start of imputing missing values...')
+            logging.info('Start of imputing missing values...')
             imputer=KNNImputer(n_neighbors=3, weights='uniform',missing_values=np.nan)
             self.new_array=imputer.fit_transform(self.data) # impute the missing values
             # convert the nd-array returned in the step above to a Data frame
             self.new_data=pd.DataFrame(data=self.new_array, columns=self.data.columns)
-            self.logger.info('End of imputing missing values...')
+            logging.info('End of imputing missing values...')
             return self.new_data
         except Exception as e:
-            self.logger.exception('Exception raised while imputing missing values:'+str(e))
-            raise Exception()
+            logging.info('Exception raised while imputing missing values')
+            raise CustomException(e,sys)
 
     def feature_encoding(self, data):
         """
@@ -140,25 +131,22 @@ class Preprocessor:
         * description: method to impute missing values
         * return: none
         *
-        * who             when           version  change (include bug# if apply)
-        * ----------      -----------    -------  ------------------------------
-        * bcheekati       05-MAY-2020    1.0      initial creation
         *
         * Parameters
         *   data:
         """
         try:
-            self.logger.info('Start of feature encoding...')
+            logging.info('Start of feature encoding...')
             self.new_data = data.select_dtypes(include=['object']).copy()
             # Using the dummy encoding to encode the categorical columns to numerical ones
             for col in self.new_data.columns:
                 self.new_data = pd.get_dummies(self.new_data, columns=[col], prefix=[col], drop_first=True)
 
-            self.logger.info('End of feature encoding...')
+            logging.info('End of feature encoding...')
             return self.new_data
         except Exception as e:
-            self.logger.exception('Exception raised while feature encoding:' + str(e))
-            raise Exception()
+            logging.exception('Exception raised while feature encoding:' + str(e))
+            raise CustomException(e,sys)
 
 
     def split_features_label(self, data, label_name):
@@ -167,9 +155,6 @@ class Preprocessor:
         * description: method to separate features and label
         * return: none
         *
-        * who             when           version  change (include bug# if apply)
-        * ----------      -----------    -------  ------------------------------
-        * bcheekati       05-MAY-2020    1.0      initial creation
         *
         * Parameters
         *   data:
@@ -177,14 +162,14 @@ class Preprocessor:
         """
         self.data =data
         try:
-            self.logger.info('Start of splitting features and label ...')
+            logging.info('Start of splitting features and label ...')
             self.X=self.data.drop(labels=label_name,axis=1) # drop the columns specified and separate the feature columns
             self.y=self.data[label_name] # Filter the Label columns
-            self.logger.info('End of splitting features and label ...')
+            logging.info('End of splitting features and label ...')
             return self.X,self.y
         except Exception as e:
-            self.logger.exception('Exception raised while splitting features and label:' + str(e))
-            raise Exception()
+            logging.info('Exception raised while splitting features and label')
+            raise CustomException(e,sys)
 
     def final_predictset(self,data):
         """
@@ -192,32 +177,23 @@ class Preprocessor:
         * description: method to build final predict set by adding additional encoded column with value as 0
         * return: column_names, Number of Columns
         *
-        * who             when           version  change (include bug# if apply)
-        * ----------      -----------    -------  ------------------------------
-        * bcheekati       05-MAY-2020    1.0      initial creation
         *
         * Parameters
         *   none:
         """
         try:
-            self.logger.info('Start of building final predictset...')
+            logging.info('Start of building final predictset...')
             with open('apps/database/columns.json', 'r') as f:
                 data_columns = json.load(f)['data_columns']
                 f.close()
             df = pd.DataFrame(data=None, columns=data_columns)
             df_new = pd.concat([df, data], ignore_index=True,sort=False)
             data_new = df_new.fillna(0)
-            self.logger.info('End of building final predictset...')
+            logging.info('End of building final predictset...')
             return data_new
-        except ValueError:
-            self.logger.exception('ValueError raised while building final predictset')
-            raise ValueError
-        except KeyError:
-            self.logger.exception('KeyError raised while building final predictset')
-            raise KeyError
         except Exception as e:
-            self.logger.exception('Exception raised while building final predictset: %s' % e)
-            raise e
+            logging.info('Exception raised while building final predictset')
+            raise CustomException(e,sys)
 
 
     def preprocess_trainset(self):
@@ -226,15 +202,12 @@ class Preprocessor:
         * description: method to pre-process training data
         * return: none
         *
-        * who             when           version  change (include bug# if apply)
-        * ----------      -----------    -------  ------------------------------
-        * bcheekati       05-MAY-2020    1.0      initial creation
         *
         * Parameters
         *   none:
         """
         try:
-            self.logger.info('Start of Preprocessing...')
+            logging.info('Start of Preprocessing...')
             # get data into pandas data frame
             data=self.get_data()
             # drop unwanted columns
@@ -251,11 +224,11 @@ class Preprocessor:
                 data = self.impute_missing_values(data)  # missing value imputation
             # create separate features and labels
             self.X, self.y = self.split_features_label(data, label_name='left')
-            self.logger.info('End of Preprocessing...')
+            logging.info('End of Preprocessing...')
             return self.X, self.y
-        except Exception:
-            self.logger.exception('Unsuccessful End of Preprocessing...')
-            raise Exception
+        except Exception as e:
+            logging.info('Unsuccessful End of Preprocessing...')
+            raise CustomException(e,sys)
 
     def preprocess_predictset(self):
         """
@@ -263,15 +236,12 @@ class Preprocessor:
         * description: method to pre-process prediction data
         * return: none
         *
-        * who             when           version  change (include bug# if apply)
-        * ----------      -----------    -------  ------------------------------
-        * bcheekati       05-MAY-2020    1.0      initial creation
         *
         * Parameters
         *   none:
         """
         try:
-            self.logger.info('Start of Preprocessing...')
+            logging.info('Start of Preprocessing...')
             # get data into pandas data frame
             data=self.get_data()
             # drop unwanted columns
@@ -288,11 +258,11 @@ class Preprocessor:
                 data = self.impute_missing_values(data)  # missing value imputation
 
             data = self.final_predictset(data)
-            self.logger.info('End of Preprocessing...')
+            logging.info('End of Preprocessing...')
             return data
-        except Exception:
-            self.logger.exception('Unsuccessful End of Preprocessing...')
-            raise Exception
+        except Exception as e:
+            logging.info('Unsuccessful End of Preprocessing...')
+            raise CustomException(e,sys)
 
 
     def preprocess_predict(self,data):
@@ -309,7 +279,7 @@ class Preprocessor:
         *   none:
         """
         try:
-            self.logger.info('Start of Preprocessing...')
+            logging.info('Start of Preprocessing...')
             cat_df = self.feature_encoding(data)
             data = pd.concat([data, cat_df], axis=1)
             # drop categorical column
@@ -321,8 +291,8 @@ class Preprocessor:
                 data = self.impute_missing_values(data)  # missing value imputation
 
             data = self.final_predictset(data)
-            self.logger.info('End of Preprocessing...')
+            logging.info('End of Preprocessing...')
             return data
-        except Exception:
-            self.logger.exception('Unsuccessful End of Preprocessing...')
-            raise Exception
+        except Exception as e:
+            logging.info('Unsuccessful End of Preprocessing...')
+            raise CustomException(e,sys)
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,11 @@
 from datetime import datetime
 import random
+import pickle
+import os
+import shutil
+from src.logger import logging
+from src.exception import CustomException
+import sys
 
 class Config:
     """
@@ -39,3 +45,107 @@ class Config:
         self.date = self.now.date()
         self.current_time = self.now.strftime("%H%M%S")
         return str(self.date)+"_"+str(self.current_time)+"_"+str(random.randint(100000000, 999999999))
+    
+
+class FileOperation:
+    """
+    *****************************************************************************
+    *
+    * file_name:       FileOperation.py
+    * version:        1.0
+    * author:         CODESTUDIO
+    * creation date:  05-MAY-2020
+    *
+    * change history:
+    *
+    *
+    *
+    * description:    Class for file operation
+    *
+    ****************************************************************************
+    """
+
+    def __init__(self,run_id,data_path,mode):
+        self.run_id = run_id
+        self.data_path = data_path
+
+    def save_model(self,model,file_name):
+        """
+        * method: save_model
+        * description: method to save the model file
+        * return: File gets saved
+        *
+        *
+        * Parameters
+        *   model:
+        *   file_name:
+        """
+        try:
+            logging.info('Start of Save Models')
+            path = os.path.join('apps/models/',file_name) #create seperate directory for each cluster
+            if os.path.isdir(path): #remove previously existing models for each clusters
+                shutil.rmtree('apps/models')
+                os.makedirs(path)
+            else:
+                os.makedirs(path) #
+            with open(path +'/' + file_name+'.sav',
+                      'wb') as f:
+                pickle.dump(model, f) # save the model to file
+            logging.info('Model File '+file_name+' saved')
+            logging.info('End of Save Models')
+            return 'success'
+        except Exception as e:
+            logging.info('Exception raised while Save Models')
+            raise CustomException(e,sys)
+
+    def load_model(self,file_name):
+        """
+        * method: load_model
+        * description: method to load the model file
+        * return: File gets saved
+        *
+        * who             when           version  change (include bug# if apply)
+        * ----------      -----------    -------  ------------------------------
+        * bcheekati       05-MAY-2020    1.0      initial creation
+        *
+        * Parameters
+        *   file_name:
+        """
+        try:
+            logging.info('Start of Load Model')
+            with open('apps/models/' + file_name + '/' + file_name + '.sav','rb') as f:
+                logging.info('Model File ' + file_name + ' loaded')
+                logging.info('End of Load Model')
+                return pickle.load(f)
+        except Exception as e:
+            logging.info('Exception raised while Loading Model')
+            raise CustomException(e,sys)
+
+    def correct_model(self,cluster_number):
+        """
+        * method: correct_model
+        * description: method to find best model
+        * return:  The Model file
+        *
+        *
+        * Parameters
+        *   cluster_number:
+        """
+        try:
+            logging.info('Start of finding correct model')
+            self.cluster_number= cluster_number
+            self.folder_name='apps/models'
+            self.list_of_model_files = []
+            self.list_of_files = os.listdir(self.folder_name)
+            for self.file in self.list_of_files:
+                try:
+                    if (self.file.index(str( self.cluster_number))!=-1):
+                        self.model_name=self.file
+                except:
+                    continue
+            self.model_name=self.model_name.split('.')[0]
+            logging.info('End of finding correct model')
+            return self.model_name
+        except Exception as e:
+            logging.info('Exception raised while finding correct model' )
+            raise CustomException(e,sys)
